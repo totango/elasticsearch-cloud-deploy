1473417ab81388c9eb71be39978a5c484d1a09f0
ronenbdbq <123952033+ronenbdbq@users.noreply.github.com>
2023-02-18 17:29:18 +1100

    A number of small fixes related to newer Ubuntu release and packer changes

--- packer/update-machine.sh
+++ packer/update-machine.sh
@@ -6,9 +6,9 @@ sudo rm /boot/grub/menu.lst
 sudo apt-get upgrade -y
 sudo apt-get install -y software-properties-common git python-dev htop ntp jq apt-transport-https
 
+if [[ $PACKER_BUILD_NAME == "aws" ]]; then
+      sudo -E apt-get install -y awscli
+fi
+
 # Disable daily apt unattended updates.
 echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
-
-wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py || python2 get-pip.py
-pip install boto awscli || pip2 install boto awscli 
