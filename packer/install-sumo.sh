0dac5c8f85dd03da4218ea1ff399bcc0cd9c9193
ronenbdbq <123952033+ronenbdbq@users.noreply.github.com>
2023-03-24 01:26:02 +1100

    sumo fix

--- packer/install-sumo.sh
+++ packer/install-sumo.sh
@@ -3,4 +3,5 @@ set -e
 
 curl -o /tmp/SumoCollector.sh https://collectors.us2.sumologic.com/rest/download/linux/64
 chmod +x /tmp/SumoCollector.sh
-sudo /tmp/SumoCollector.sh -q -Vsumo.token_and_url=foobar
+sudo /tmp/SumoCollector.sh -q -Vsumo.accessid=$SUMO_ACCESS_ID -Vsumo.accesskey=$SUMO_ACCESS_KEY
+
